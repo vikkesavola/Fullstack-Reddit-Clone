d9762c00522ba00fa20d7cfbb75ceb8186fefa4b
Vikke <vikke.savola@gmail.com>
2026-08-05 18:29:44 +0300

    Add loading state management to homepage component

--- client/src/lib/states/homePageState.svelte.js
+++ client/src/lib/states/homePageState.svelte.js
@@ -2,16 +2,22 @@ import { browser } from "$app/environment";
 import * as postsApi from "$lib/apis/postsApi.js";
 
 let homepageState = $state([]);
+let isLoading = $state(true);
 
 const initHomepage = async (communityId) => {
   if (!browser) return;
+  isLoading = true;
   homepageState = await postsApi.getHomepagePosts();
+  isLoading = false;
 };
 
 const useHomepageState = () => {
   return {
     get posts() {
       return homepageState
+    },
+    get loading() {
+      return isLoading;
     },
     updatePost(updatedPost) {
       homepageState = homepageState.map((p) =>
